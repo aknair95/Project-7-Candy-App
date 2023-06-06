afd75cb458aece135135e213952354b7f9120892
Aakash Nair <123412529+aknair95@users.noreply.github.com>
2023-06-06 21:02:16 +0530

    header component called in app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 
-import './App.css';
+import { Fragment } from 'react';
+import Header from './components/layout/header';
 
 function App() {
-  
+  return(
+    <Fragment>
+      <Header/>
+    </Fragment>
+  )
 }
 
 export default App;
