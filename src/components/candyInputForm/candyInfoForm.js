cfb0fd1456eff255225c3eed6c8031d61b4e34d2
Aakash Nair <123412529+aknair95@users.noreply.github.com>
2023-06-09 02:12:29 +0530

    candy user form component updated

--- src/components/candyInputForm/candyInfoForm.js
+++ src/components/candyInputForm/candyInfoForm.js
@@ -3,6 +3,7 @@ import { useRef } from "react";
 import Card from "../UI/card";
 import classes from "./candyInfoForm.module.css";
 
+
 const CandyInfoForm=(props) =>{
     
     const candyNameRef=useRef();
@@ -12,13 +13,12 @@ const CandyInfoForm=(props) =>{
     const addCandyBtnHandler=(e) =>{
         e.preventDefault();
         const enteredName=candyNameRef.current.value;
-        const enteredDescription=candyNameRef.current.value;
+        const enteredDescription=candyDescriptionRef.current.value;
         const enteredPrice=candyPriceRef.current.value;
       
-        const newCandyData={Name: enteredName,Description: enteredDescription,Price: enteredPrice};
-
-        //props.onAddCandyData(newCandyData);
-        
+        const newCandyData={name: enteredName,description: enteredDescription,price: enteredPrice,qty: 0};
+        props.onAddCandyData(newCandyData);
+    
         candyNameRef.current.value="";
         candyDescriptionRef.current.value="";
         candyPriceRef.current.value="";
@@ -26,12 +26,13 @@ const CandyInfoForm=(props) =>{
     return (
         <Card>
             <form className={classes["form-control"]}> 
+                <h2>CANDY DETAIL</h2>
                 <label htmlFor="name">Candy Name:</label>
                 <input type="text" id="name" ref={candyNameRef}/>
                 <label htmlFor="decription" >Description:</label>
-                <input id="description" ref={candyDescriptionRef}/>
+                <input type="text" id="description" ref={candyDescriptionRef}/>
                 <label htmlFor="price">Price(Rs):</label>
-                <input type="number" id="price" ref={candyPriceRef}/>
+                <input type="number" id="price" min="1" ref={candyPriceRef}/>
                 <button id="addBtn" onClick={addCandyBtnHandler}>ADD CANDY</button>
             </form>
         </Card>    
